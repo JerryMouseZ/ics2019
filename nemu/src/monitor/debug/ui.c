64f91c1ded3d838534c9d5e8b3753b1b80fb6f5b
tracer-ics2019 <tracer@njuics.org>
2020-02-20 05:56:06 +0800

    >  compile 181220000 jz Linux jiangzhang 5.3.0-28-generic #30~18.04.1-Ubuntu SMP Fri Jan 17 06:14:09 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:56:06 up  6:08,  1 user,  load average: 1.31, 1.21, 1.35 5f9ed232f6e28664127476d8e4b8724d656390f

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -98,6 +98,7 @@ static int cmd_x(char *args)
 
   for (int i = 0; i < n; i++){
     printf("0x%08x ", addr);
+    //printf("0x%08x\n", paddr_read(addr, 4));
     printf("0x%08x\n", vaddr_read(addr, 4));
     addr += 4;
   }
