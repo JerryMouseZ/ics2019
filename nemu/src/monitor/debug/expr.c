0280e6e30a5183ef620113592b604a5f5824dc3a
tracer-ics2019 <tracer@njuics.org>
2020-03-03 19:55:36 +0800

    >  compile 181220000 jz Linux jiangzhang 5.3.0-41-generic #33~18.04.1-Ubuntu SMP Mon Feb 17 12:51:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:55:36 up  2:57,  1 user,  load average: 1.00, 1.14, 1.02 61b718b04d82c5f0894d66956f127cc25b68a8ee

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -259,14 +259,6 @@ uint32_t expr(char *e, bool *success)
     }
   }
   *success = true;
-  for (int i = 0; i <= values_top; i++)
-  {
-    printf("%d\n", values[i]);
-  }
-  for (int i = 0; i <= ops_top; i++)
-  {
-    printf("%c\n", ops[i]);
-  }
   while (values_top > 0)
   {
     int tmp = 0;
