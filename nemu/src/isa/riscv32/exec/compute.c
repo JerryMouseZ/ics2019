731a99b5c840039267533ee65c6f12f316152684
JJeJZ <jiangzhangsg@qq.com>
2020-03-18 21:36:02 +0800

    remote other ISA

--- nemu/src/isa/riscv32/exec/compute.c
+++
@@ -1,7 +0,0 @@
-#include "cpu/exec.h"
-
-make_EHelper(lui) {
-  rtl_sr(id_dest->reg, &id_src->val, 4);
-
-  print_asm_template2(lui);
-}
--- nemu/src/isa/riscv32/include/isa/diff-test.h
+++
@@ -1,6 +0,0 @@
-#ifndef __RISCV32_DIFF_TEST_H__
-#define __RISCV32_DIFF_TEST_H__
-
-#define DIFFTEST_REG_SIZE (sizeof(uint32_t) * 33) // GRPs + pc
-
-#endif
