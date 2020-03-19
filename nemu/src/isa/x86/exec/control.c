cc5f4152e905a2dfe61bbab5b622c9e93e578b3e
tracer-ics2019 <tracer@njuics.org>
2020-03-19 19:12:45 +0800

    >  compile 181220000 jz Linux jiangzhang 5.3.0-42-generic #34~18.04.1-Ubuntu SMP Fri Feb 28 13:42:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:12:45 up 11:48,  1 user,  load average: 1.35, 1.17, 1.01 45bc58dbfef42d1effd8b5d3d93f5b05a8c23107

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -30,7 +30,7 @@ make_EHelper(call) {
   // printf("is_jmp: %d\n",decinfo.is_jmp);
   decinfo.is_jmp = 1;
   printf("push seq_pc\n");
-  rtl_push(decinfo.seq_pc);
+  rtl_push(&decinfo.seq_pc);
   print_asm("call %x", decinfo.jmp_pc);
 }
 
