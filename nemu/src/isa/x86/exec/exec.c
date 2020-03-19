1762cd796ae7bdd71d22b7083f485854d3199a28
tracer-ics2019 <tracer@njuics.org>
2020-03-19 19:21:17 +0800

    >  compile 181220000 jz Linux jiangzhang 5.3.0-42-generic #34~18.04.1-Ubuntu SMP Fri Feb 28 13:42:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:21:17 up 11:56,  1 user,  load average: 0.54, 0.69, 0.82 6e18268d53a4f11358fd51994c401153b5178021

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -76,7 +76,7 @@ static OpcodeEntry opcode_table [512] = {
   // 0x55 push 要知道push的译码函数
   /* 0x54 */	EMPTY, IDEX(push_SI,push), EMPTY, EMPTY,
   /* 0x58 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x5c */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x5c */	EMPTY, IDEX(push_SI, pop), EMPTY, EMPTY,
   /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x64 */	EMPTY, EMPTY, EX(operand_size), EMPTY,
   /* 0x68 */	EMPTY, EMPTY, EMPTY, EMPTY,
@@ -147,7 +147,7 @@ static OpcodeEntry opcode_table [512] = {
   /* 0x54 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x58 */	EMPTY, EMPTY, EMPTY, EMPTY,
   //0x5d pop
-  /* 0x5c */	EMPTY, IDEX(push_SI, pop), EMPTY, EMPTY,
+  /* 0x5c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x64 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x68 */	EMPTY, EMPTY, EMPTY, EMPTY,
