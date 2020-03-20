18849851b9d123216f86523ddf284cbf3a3130f6
tracer-ics2019 <tracer@njuics.org>
2020-03-20 22:49:11 +0800

    >  compile 181220000 jz Linux jiangzhang 5.3.0-42-generic #34~18.04.1-Ubuntu SMP Fri Feb 28 13:42:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:49:11 up 11:28,  1 user,  load average: 1.11, 0.78, 0.78 356a8078c1a41dc2f952647e47c6f09cbc41a37

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -58,7 +58,8 @@ static inline void update_pc(void)
   if (decinfo.is_jmp)
   {
     decinfo.is_jmp = 0;
-    // cpu.pc = decinfo.jmp_pc;
+    cpu.pc = decinfo.jmp_pc;
+    printf("jmp %x\n", decinfo.jmp_pc);
   }
   else
   {
