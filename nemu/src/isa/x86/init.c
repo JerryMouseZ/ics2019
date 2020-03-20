9ee33c5c67534b3a6ff06af81a33044e9fd5fd0a
tracer-ics2019 <tracer@njuics.org>
2020-03-20 19:53:44 +0800

    >  compile 181220000 jz Linux jiangzhang 5.3.0-42-generic #34~18.04.1-Ubuntu SMP Fri Feb 28 13:42:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:53:44 up  8:33,  1 user,  load average: 0.85, 0.99, 0.93 7362c68f4e302c8cbcd312388fa07f8185b9763

--- nemu/src/isa/x86/init.c
+++ nemu/src/isa/x86/init.c
@@ -16,6 +16,8 @@ const long isa_default_img_size = sizeof(isa_default_img);
 static void restart() {
   /* Set the initial program counter. */
   cpu.pc = PC_START;
+  // init EFLAGS
+
 }
 
 void init_isa(void) {
