a7f6f009418169d0ece249d7604ade5ec79c0056
tracer-ics2019 <tracer@njuics.org>
2020-03-21 10:57:04 +0800

    >  compile 181220000 jz Linux jiangzhang 5.3.0-42-generic #34~18.04.1-Ubuntu SMP Fri Feb 28 13:42:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:57:04 up 23:36,  1 user,  load average: 2.02, 1.50, 1.27 d41ccd76ab7efec753dd7048cce791de877995

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -41,8 +41,8 @@ make_group(gp4,
 
 /* 0xff */
 make_group(gp5,
-    EX(push), EMPTY, EMPTY, EMPTY,
-    EMPTY, EMPTY, EMPTY, EMPTY)
+    EX(push), EX(push), EX(push), EX(push),
+    EX(push), EX(push), EX(push), EX(push))
 
 /* 0x0f 0x01*/
 make_group(gp7,
