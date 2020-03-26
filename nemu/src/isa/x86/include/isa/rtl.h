b048a4f3a801a1c454c034b0203cb6a565e4fd78
tracer-ics2019 <tracer@njuics.org>
2020-03-26 23:06:10 +0800

    >  compile 181220000 jz Linux jiangzhang 5.3.0-42-generic #34~18.04.1-Ubuntu SMP Fri Feb 28 13:42:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:06:09 up  8:20,  1 user,  load average: 0.54, 0.59, 0.69 2ed8f5ac3e172f176a5f5828b22841d45b5b7da

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -45,16 +45,16 @@ static inline void rtl_push(const rtlreg_t *src1)
 {
   // esp <- esp - 4
   // M[esp] <- src1
-  printf("0x%x\n", cpu.esp);
+  printf("esp: 0x%x\n", cpu.esp);
   cpu.esp = cpu.esp - 4;
   vaddr_write(cpu.esp, *src1, 4);
-  printf("checkpoint2\n");
 }
 
 static inline void rtl_pop(rtlreg_t *dest)
 {
   // dest <- M[esp]
   // esp <- esp + 4
+  printf("esp: 0x%x\n", cpu.esp);
   *dest = vaddr_read(cpu.esp, 4);
   cpu.esp = cpu.esp + 4;
 }
