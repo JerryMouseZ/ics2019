29a52fd65ac52197abd80cc88af41abd8cad5480
tracer-ics2019 <tracer@njuics.org>
2020-03-26 23:13:18 +0800

    >  compile 181220000 jz Linux jiangzhang 5.3.0-42-generic #34~18.04.1-Ubuntu SMP Fri Feb 28 13:42:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:13:18 up  8:27,  1 user,  load average: 0.91, 0.64, 0.66 67345875f1eddde6357b8604c24daf4a60c2f4

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -11,6 +11,7 @@ make_EHelper(test)
 make_EHelper(and)
 {
   // TODO();
+  printf("esp : 0x%x\n", cpu.esp);
   rtl_and(&id_dest->val,&id_dest->val, &id_src->val);
   operand_write(id_dest, &id_dest->val);
   rtl_update_ZFSF(&id_dest->val, id_dest->width);
