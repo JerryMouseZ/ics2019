dffa0fd20d6625a021eca27bb6d940b977bfb636
JJeJZ <jiangzhangsg@qq.com>
2020-05-15 17:25:48 +0800

    cte structure

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -71,6 +71,8 @@ static int cmd_info(char *args)
       printf("%s 0x%08x\n", reg_name(i, 4), cpu.gpr[i]._32);
     }
     printf("%s 0x%08x\n", "eip", cpu.pc);
+    printf("%s 0x%08x\n", "eflags", cpu.eflag);
+    printf("%s 0x%08x\n", "cs", cpu.cs);
     // for (int i = R_AX; i < R_DI; i++)
     // {
     //   printf("%s 0x%04x\n", reg_name(i, 2), reg_w(i));
--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -1,23 +1,41 @@
 #include <am.h>
+#include <klib.h>
 #include <x86.h>
-
-static _Context* (*user_handler)(_Event, _Context*) = NULL;
+static _Context *(*user_handler)(_Event, _Context *) = NULL;
 
 void __am_irq0();
 void __am_vecsys();
 void __am_vectrap();
 void __am_vecnull();
 
-_Context* __am_irq_handle(_Context *c) {
+void print_context(_Context *c)
+{
+  void *begin = c;
+  while (begin < (void *)c + sizeof(_Context))
+  {
+    printf("0x%x\n", *(int *)begin);
+    begin += 4;
+  }
+  printf("--------------\n");
+}
+
+_Context *__am_irq_handle(_Context *c)
+{
   _Context *next = c;
-  if (user_handler) {
+  print_context(c);
+  if (user_handler)
+  {
     _Event ev = {0};
-    switch (c->irq) {
-      default: ev.event = _EVENT_ERROR; break;
+    switch (c->irq)
+    {
+    default:
+      ev.event = _EVENT_ERROR;
+      break;
     }
 
     next = user_handler(ev, c);
-    if (next == NULL) {
+    if (next == NULL)
+    {
       next = c;
     }
   }
@@ -25,14 +43,16 @@ _Context* __am_irq_handle(_Context *c) {
   return next;
 }
 
-int _cte_init(_Context*(*handler)(_Event, _Context*)) {
+int _cte_init(_Context *(*handler)(_Event, _Context *))
+{
   static GateDesc idt[NR_IRQ];
   // initialize IDT
-  for (unsigned int i = 0; i < NR_IRQ; i ++) {
+  for (unsigned int i = 0; i < NR_IRQ; i++)
+  {
     idt[i] = GATE(STS_TG32, KSEL(SEG_KCODE), __am_vecnull, DPL_KERN);
   }
   // ----------------------- interrupts ----------------------------
-  idt[32]   = GATE(STS_IG32, KSEL(SEG_KCODE), __am_irq0,   DPL_KERN);
+  idt[32] = GATE(STS_IG32, KSEL(SEG_KCODE), __am_irq0, DPL_KERN);
   // ---------------------- system call ----------------------------
   idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), __am_vecsys, DPL_USER);
   idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), __am_vectrap, DPL_KERN);
@@ -42,17 +62,21 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
   return 0;
 }
 
-_Context *_kcontext(_Area stack, void (*entry)(void *), void *arg) {
+_Context *_kcontext(_Area stack, void (*entry)(void *), void *arg)
+{
   return NULL;
 }
 
-void _yield() {
+void _yield()
+{
   asm volatile("int $0x81");
 }
 
-int _intr_read() {
+int _intr_read()
+{
   return 0;
 }
 
-void _intr_write(int enable) {
+void _intr_write(int enable)
+{
 }
