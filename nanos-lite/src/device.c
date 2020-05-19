a62aac0856f03d4906928e6f3549c499b3e4ce6d
JJeJZ <jiangzhangsg@qq.com>
2020-05-19 16:42:26 +0800

    debugging pal

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,7 +25,7 @@ static const char *keyname[256] __attribute__((used)) = {
 size_t events_read(void *buf, size_t offset, size_t len)
 {
   int ret = 0;
-  char rec_buf[40];
+  char rec_buf[1024] = {0};
   int key = read_key();
   int down = 0;
   if (key & 0x8000)
