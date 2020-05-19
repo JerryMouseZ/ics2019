a144630a6a7f6e610c4262aca3f92363f2f2b07c
JJeJZ <jiangzhangsg@qq.com>
2020-05-19 14:26:44 +0800

    debugging pal

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -85,6 +85,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len)
 size_t fbsync_write(const void *buf, size_t offset, size_t len)
 {
   draw_sync();
+  return len;
 }
 
 void init_device()
