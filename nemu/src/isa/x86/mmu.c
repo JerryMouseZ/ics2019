f20e81630143f9def26a9fde702a2a7727847455
JJeJZ <jiangzhangsg@qq.com>
2020-05-28 18:33:37 +0800

    change address space

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -54,14 +54,22 @@ void isa_vaddr_write(vaddr_t addr, uint32_t data, int len)
 
   if (((addr + len - 1) & PAGE) != (addr & PAGE))
   {
-    uint32_t temp = (addr + len - 1) & PAGE;
-    int l1 = temp - addr, l2 = addr + len - temp;
-    uint32_t paddr1 = page_translate(addr, l1), paddr2 = page_translate(temp, l2);
-    //写低位
-    paddr_write(paddr1, (data << l2) >> l2, l1);
-    //写高位
-    paddr_write(paddr2, (data >> l1), l2);
+    uint32_t paddr = 0;
+    for (int i = 0; i < len; i++)
+    {
+      paddr = page_translate(addr + i, true);
+      paddr_write(paddr, data & 0xf, 1);
+      data >>= 8;
+    }
     return;
+    // uint32_t temp = (addr + len - 1) & PAGE;
+    // int l1 = temp - addr, l2 = addr + len - temp;
+    // uint32_t paddr1 = page_translate(addr, l1), paddr2 = page_translate(temp, l2);
+    // //写低位
+    // paddr_write(paddr1, (data << (l2 * 8)) >> (l2 * 8), l1);
+    // //写高位
+    // paddr_write(paddr2, (data >> (l1 * 8)), l2);
+    // return;
   }
   paddr_t paddr = page_translate(addr, 1);
   paddr_write(paddr, data, len);
