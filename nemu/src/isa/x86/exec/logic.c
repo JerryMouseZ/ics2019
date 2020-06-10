c2dee5638625b3cbda88890d2354e68b8d4d0651
jiangzhang <jiangzhangsg@qq.com>
2020-06-10 12:56:56 +0800

    fix sar

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -61,7 +61,14 @@ make_EHelper(sar)
 {
   // TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_sar(&s0, &id_dest->val, &id_src->val);
+  if (id_dest->width == 1)
+  {
+    id_dest->val = (int8_t)id_dest->val;
+  }
+  else if (id_dest->width == 2)
+  {
+    id_dest->val = (int16_t)id_dest->val;
+  }
   rtl_update_ZFSF(&s0, id_dest->width);
   operand_write(id_dest, &s0);
   print_asm_template2(sar);
